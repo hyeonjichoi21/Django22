1c0b6df8dee50301a21de280d78bcca2b4dd1b7c
hyeonjichoi21 <ch5i_hj15@naver.com>
2022-11-03 12:15:51 +0900

    다대일 관계 구현하기 완료

--- blog/urls.py
+++ blog/urls.py
@@ -3,7 +3,8 @@ from . import views
 
 urlpatterns = [  # IP주소/blog/
     path('', views.PostList.as_view()),
-    path('<int:pk>/', views.PostDetail.as_view())
+    path('<int:pk>/', views.PostDetail.as_view()),
+    path('category/<str:slug>/', views.category_page)
     #path('', views.index), #  IP주소/blog/
     #path('<int:pk>/', views.single_post_page)
 ]
